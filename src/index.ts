2509f7c1e2b76dfc3033a6ffd5fc3eaa75829e54
Christian-Kammerath <Christian-Kammerath@gmx.de>
2023-12-15 16:41:54 +0100

    new CommandLineParser

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const commands = new CommandLineParser(process.argv.slice(2))
 
 ConnectDatabase(commands.DatabaseUrl,commands.DatabaseUser,commands.DatabasePassword,commands.DatabasePort,commands.DatabaseName);
 
-
+ 
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
